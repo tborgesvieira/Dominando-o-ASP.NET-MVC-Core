b5d95143079c03d8c76eeedb1c341e8b66070152
Thiago Borges <thiagoborges@amhp.com.br>
2019-12-19 15:56:36 -0300

    Ajuste para incluir o fornecedorid da aula anterior

--- MinhaAppMvcCompleta/src/DevIO.App/Models/ProdutoViewModel.cs
+++ MinhaAppMvcCompleta/src/DevIO.App/Models/ProdutoViewModel.cs
@@ -9,6 +9,11 @@ namespace DevIO.App.Models
     {
         [Key]
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DisplayName("Fornecedor")]
+        public Guid FornecedorId { get; set; }
+
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Nome { get; set; }
