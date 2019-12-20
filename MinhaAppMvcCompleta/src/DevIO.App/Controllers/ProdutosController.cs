25ac9febcd95da53ebecbec18b3b74b86b7ca281
Thiago Borges <thiagoborges@amhp.com.br>
2019-12-20 08:40:09 -0300

    Aula - Upload de imagem do produto

--- MinhaAppMvcCompleta/src/DevIO.App/Controllers/ProdutosController.cs
+++ MinhaAppMvcCompleta/src/DevIO.App/Controllers/ProdutosController.cs
@@ -2,9 +2,11 @@
 using DevIO.App.Models;
 using DevIO.Business.Interfaces;
 using DevIO.Business.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace DevIO.App.Controllers
@@ -55,10 +57,16 @@ namespace DevIO.App.Controllers
 
             if (!ModelState.IsValid) View(produtoViewModel);
 
+            var imgPrefixo = Guid.NewGuid() + "_";
+
+            if (!await UploadArquivo(produtoViewModel.ImagemUpload, imgPrefixo)) View(produtoViewModel);
+
+            produtoViewModel.Imagem = imgPrefixo + produtoViewModel.ImagemUpload.FileName;
+
             await _produtoRepository.Adicionar(_mapper.Map<Produto>(produtoViewModel));
 
-            return View(produtoViewModel);
-        }
+            return RedirectToAction("Index");
+        }        
 
         public async Task<IActionResult> Edit(Guid id)
         {
@@ -121,6 +129,25 @@ namespace DevIO.App.Controllers
             produto.Fornecedores = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
 
             return produto;
+        }
+
+        private async Task<bool> UploadArquivo(IFormFile arquivo, string imgPrefixo)
+        {
+            if (arquivo.Length <= 0) return false;
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagens", imgPrefixo + arquivo.FileName);
+
+            if (System.IO.File.Exists(path))
+            {
+                ModelState.AddModelError(string.Empty, "Já existe um arquivo com esse nome!");
+                return false;
+            }
+
+            using var strem = new FileStream(path, FileMode.Create);
+            
+            await arquivo.CopyToAsync(strem);
+
+            return true;
         }
     }
 }
